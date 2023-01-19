29db43d67d23085700b395d5c9919eee788d7f90
NathanD0wd <dowdnj@gmail.com>
2023-01-19 15:18:23 -0800

    Update StringURL.java

--- StringURL.java
+++ StringURL.java
@@ -3,7 +3,7 @@ import java.net.URI;
 import java.util.ArrayList;
 
 class Handler implements URLHandler {
-    // The one bit of state on the server: a number that will be manipulated by
+    // The one bit of state on the server: a list that will be manipulated by
     // various requests.
     ArrayList<String> list = new ArrayList<String>();
 
@@ -19,6 +19,7 @@ class Handler implements URLHandler {
             list.add( url.getQuery());
             return url.getQuery() + " added";
         } else if ( url.getPath().equals("/search") ){
+            output += "List of words matching your search:\n";
             for ( int i = 0 ; i < list.size() ; i++ ){
                 if ( list.get(i).contains( url.getQuery() ) ){
                     output += list.get(i) + ", ";
@@ -30,7 +31,7 @@ class Handler implements URLHandler {
     }
 }
 
-class NumberServer {
+class StringURL {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
